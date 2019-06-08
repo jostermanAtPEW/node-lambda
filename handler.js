4b64fac1bd7dbed9b603a51e9eed783a9bcd4838
John Osterman <josterman@pewtrusts.org>
2019-06-07 21:26:35 -0400

    uses require https

--- handler.js
+++ handler.js
@@ -1,14 +1,32 @@
 'use strict';
-
+const https = require('https');
 const Papa = require('papaparse');
+
+
 const data = function(){
     return new Promise(function(resolve) {
-        Papa.parse("https://www.pewtrusts.org/~/media/data-visualizations/interactives/2018/EIFP/data/countries.csv?v=e6bdba", {
+        https.get('https://www.pewtrusts.org/~/media/data-visualizations/interactives/2018/EIFP/data/countries.csv?v=e6bdba', (resp) => {
+          let data = '';
+
+          // A chunk of data has been recieved.
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+
+          // The whole response has been received. Print out the result.
+          resp.on('end', () => {
+            resolve(data);
+          });
+
+        }).on("error", (err) => {
+          console.log("Error: " + err.message);
+        });
+        /*Papa.parse("https://www.pewtrusts.org/~/media/data-visualizations/interactives/2018/EIFP/data/countries.csv?v=e6bdba", {
             download: true,
             complete: function(results) {
                 resolve(results);
             }
-        });
+        });*/
     });
 }
 module.exports.hello = async (event, context) => {
